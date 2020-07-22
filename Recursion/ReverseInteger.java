bdda6c2017727977936e2e1db0b1d143160c0243
Pradeep Rao Banda <pradeepraob@gmail.com>
2020-07-22 22:02:53 +0530

    ReverseInteger class

---
+++ Recursion/ReverseInteger.java
@@ -0,0 +1,46 @@
+public class ReverseInteger
+{
+    public static void main(String[] args) {
+        System.out.println("3456: " + reverseInteger(3456));
+        System.out.println("3: " + reverseInteger(3));
+        System.out.println("0: " + reverseInteger(0));
+        System.out.println("100: " + reverseInteger(100));
+        System.out.println("-10: " + reverseInteger(-10));
+    }
+
+    public static int reverseInteger(int num) {
+        if(num < 10)
+        {
+            return num;
+        }
+        else
+        {
+            return ((num % 10) * tensPower(numOfDigits(num / 10)) ) + reverseInteger(num / 10);
+        }
+        // return 0;
+    }
+    private static int tensPower(int num)
+    {
+        if( num <=0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 10 * tensPower(num-1);
+        }
+        // return 0;
+    }
+    private static int numOfDigits(int num)
+    {
+        if( num < 10)
+        {
+            return 1;
+        }
+        else
+        {
+            return 1 + numOfDigits(num / 10);
+        }
+        // return 0;
+    }
+}
