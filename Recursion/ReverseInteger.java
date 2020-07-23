f332417ba3808adf76308ee4d40f557e579f9478
Pradeep Rao Banda <pradeepraob@gmail.com>
2020-07-23 07:09:19 +0530

    ReverseInteger Java class

--- Recursion/ReverseInteger.java
+++ Recursion/ReverseInteger.java
@@ -1,15 +1,15 @@
 public class ReverseInteger
 {
     public static void main(String[] args) {
-        System.out.println("3456: " + reverseInteger(3456));
-        System.out.println("3: " + reverseInteger(3));
-        System.out.println("0: " + reverseInteger(0));
-        System.out.println("100: " + reverseInteger(100));
+        // System.out.println("3456: " + reverseInteger(3456));
+        // System.out.println("3: " + reverseInteger(3));
+        // System.out.println("0: " + reverseInteger(0));
+        // System.out.println("100: " + reverseInteger(100));
         System.out.println("-10: " + reverseInteger(-10));
     }
 
     public static int reverseInteger(int num) {
-        if(num < 10)
+        if(num < 10 && num >= 0)
         {
             return num;
         }
